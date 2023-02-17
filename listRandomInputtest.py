258e58078313b90b53fa5c23831532916f47106f
PiyushSaklani <mailforgithubonly@gmail.com>
2023-02-18 01:25:33 +0530

    Prev. code with proper comments and formatting

--- listRandomInputtest.py
+++ listRandomInputtest.py
@@ -24,6 +24,13 @@ def current_time():
     now = datetime.now()
     return now.strftime("%H:%M:%S")
 
+def append_matrix(x,y,z):
+    global matrix
+    matrix[x, 0].append(y)
+    matrix[x, 1].append(z)
+    matrix[y, 0].append(x)
+    matrix[y, 1].append(z)
+
 #! Code
 #* Comment 2: Noting the code start time 
 start_time = current_time()
@@ -38,100 +45,88 @@ range_list = [[1,2],[5,8],[10,15]]
 #* Comment 5: This is a population percentage according to the age group.
 population_pct = [0.33,0.33,0.34]
 
-#* Comment 6: List which stores the values and percentage for random selection.
+#* Comment 6: This is a interaction level.
+interaction_level = [1,2,3]
+
+#* Comment 7: List which stores the values and percentage for random selection.
 all_lists, p = random_valandpct(range_list,population_pct,[],[])
 
-#* Comment 7: Create a list of random integer values.
+#* Comment 8: Create a list of random integer values.
 int_list = np.random.choice(all_lists, size=rows, p=p)
 
-#* Comment 8: Create an empty matrix.
+#* Comment 9: Create an empty matrix.
 matrix = np.empty((rows, 3), dtype=object)
 
-#* Comment 9: Assigning empty list in the 0 and 1 index.
+#* Comment 10: Assigning empty list in the 0 and 1 index.
 for i in range(rows):
     matrix[i, 0] = []
     matrix[i, 1] = []
 
-#* Comment 10: Assigning random value to the 2 index which are stored in the "int_list".
+#* Comment 11: Assigning random value to the 2 index which are stored in the "int_list".
 matrix[:, 2] = np.random.choice(int_list, size=rows)
 
-#! Writing new code
-
-interaction_level = [1,2,3]
-# print(f"{interaction_level=}")
-
+#* Comment 12: This is a list of population.
 population_alc_list = list(range(0,rows))
-# print(f"{population_alc_list=}")
-
-# population_alc_pct = [1/rows]*rows
-# print(f"{population_alc_pct=}")
-
-sumofallInteraction = 0
-for j in range(rows): sumofallInteraction += matrix[j, 2]
-print(f"\033[33m{sumofallInteraction=}")
 
 for k in range(rows):
-    # print(f"\n{k=} {matrix[k, 2]=}")
-    l = 1
     while len(matrix[k, 0]) < matrix[k, 2]:
 
+        #* Comment 13: This is a interaction and interaction level.
         relation = rm.choice(population_alc_list)
         interaction = rm.choice(interaction_level)
 
-        # print(f"{relation=} {l=}")
-        # time.sleep(0.5)
-
+        #* Comment 14: Checking that they did not have relation before and person did not get interaction with them self.
         if relation not in matrix[k, 0] and k not in matrix[relation, 0] and k != relation:
-            matrix[k, 0].append(relation)
-            matrix[k, 1].append(interaction)
-            matrix[relation, 0].append(k)
-            matrix[relation, 1].append(interaction)
+            #* Comment 15: Appending interaction and interation value in the matrix with the help of append_matrix function.
+            append_matrix(k,relation,interaction)
 
+        #* Comment 16: Checking if we want to create new population or not.
         elif all(x in matrix[k, 0] for x in population_alc_list[1:]):
-            # print(f"{population_alc_list[1:]=}")
 
+            #* Comment 17: Creating new population and combining it with initial.
             new_row = np.array([[[], [], 0]], dtype=object)
             matrix = np.append(matrix, new_row, axis=0)
 
+            #* Comment 18: Appending new population number in the population list.
             population_alc_list.append(len(matrix)-1)
 
-            matrix[k, 0].append(len(matrix)-1)
-            matrix[k, 1].append(interaction)
-            matrix[len(matrix)-1, 0].append(k)
-            matrix[len(matrix)-1, 1].append(interaction)
+            #* Comment 19: Appending interaction and interation value in the matrix with the help of append_matrix function.
+            append_matrix(k,len(matrix)-1,interaction)
 
-        np.savetxt("test_matrix.txt", matrix, delimiter=",", fmt='%s')
-        l += 1
-
+    #* Comment 20: Removing the population whose interation is completed.
     population_alc_list.remove(k)
-    # print(f"{population_alc_list=}")
-    
 
-sumofallFINALInteraction = 0
-for j in range(rows): sumofallFINALInteraction += len(matrix[j, 0])
-print(f"{sumofallFINALInteraction=}")
-print(f"Diff: \033[92m{sumofallFINALInteraction-sumofallInteraction}\033[00m")
-
-#! -- -- -- -- -- --
-
-#* Comment 11: Saving the whole matrix in the matrix.txt file.
+#* Comment 21: Saving the whole matrix in the matrix.txt file.
 np.savetxt("test_matrix.txt", matrix, delimiter=",", fmt='%s')
 
-#* Comment 12: Noting the code ending time.
+#* Comment 22: Noting the code ending time.
 end = time.time()
 end_time = current_time()
 
-#* Comment 13: Printing the Code Runtime.
+#* Comment 23: Printing the Code Runtime.
 print(f"\n\t\033[90mStarted at : {start_time}\n\tEnd at : {end_time}\n\n\t\033[36m\033[01mTime taken : {(end-start)} s\033[00m\n")
 
-#* Comment 14: Checking that random values are in correct propotion.
+#* Comment 24: Checking that random values are in correct propotion.
+#? This code can be commented
 print("\033[35mExpected\033[00m")
+
+sumofallInteraction = 0
+for j in range(rows): sumofallInteraction += matrix[j, 2]
+print(f"\033[33m{sumofallInteraction=}\033[00m")
+
 check_pct(int_list,range_list)
 
 new_int_list = []
-for m in range(len(matrix)):
-    new_int_list.append(matrix[m, 2])
+for l in range(len(matrix)):new_int_list.append(matrix[l, 2])
 
 print("-"*40)
+
 print("\033[35mCreated\033[00m")
+
+sumofallFINALInteraction = 0
+for j in range(rows): sumofallFINALInteraction += len(matrix[j, 0])
+print(f"\033[33m{sumofallFINALInteraction=}")
+print(f"Diff: \033[92m{sumofallFINALInteraction-sumofallInteraction}\033[00m")
+
 check_pct(new_int_list,range_list)
+#? -- -- -- -- -- -- -- -- -- -- 
